e87082f6f8b3e84f7c88fbe8d75858b93fa73fde
Mohammed Farjan <Mohdfarjan680@gmail.com>
2026-02-20 21:29:57 +0530

    Java Script

---
+++ p.js
@@ -0,0 +1,10 @@
+// let a = 42;                                         //Number
+// let str = "hello";                                  //String
+// let bool = true;                                    //Booleans
+// let undef;                                          //Undefined
+// let null = null;                                    //Null
+// let obj = {name:"Farjan"};                          //Object
+// let arr = [1, 2, 3];                                //Array
+// let func = function(){console.log("Hello");};       //Function
+// let sym = Symbol("id")                              //Symbol
+// let bigInt = 563564875476214587n;                   //Big Int
